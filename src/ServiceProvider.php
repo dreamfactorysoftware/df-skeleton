4bcb4e4123878e0a60f17384733957a1c79063bf
Jason Gilmore <wj@wjgilmore.com>
2021-10-05 13:32:44 -0400

    Additional fix to account for fact connector won't likely be in dreamfactory vendor dir

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -27,7 +27,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         // add routes
         /** @noinspection PhpUndefinedMethodInspection */
         if (!$this->app->routesAreCached()) {
-            include app_path('vendor/dreamfactory/df-skeleton/routes/routes.php');
+            include __DIR__ . '/../routes/routes.php';
         }
 
         $this->addMiddleware();
